c0da93aa22f3d0686fd57b739fe40677da00b3b2
Marek Felsoci <marek.felsoci@gmail.com>
2018-01-12 17:54:29 +0100

    Implemented Item type

--- item.cpp
+++ item.cpp
@@ -1,6 +1,24 @@
 #include "item.h"
 
-Item::Item()
-{
+Item::Item() {}
 
+Item::Item(std::string name, ItemType type) {
+  this->name = name;
+  this->type = type;
+}
+
+std::string Item::getName() {
+  return this->name;
+}
+
+ItemType Item::getType() {
+  return this->type;
+}
+
+void Item::setName(std::string name) {
+  this->name = name;
+}
+
+void Item::setType(ItemType type) {
+  this->type = type;
 }
--- item.h
+++ item.h
@@ -1,11 +1,24 @@
 #ifndef ITEM_H
 #define ITEM_H
 
+#include <iostream>
 
-class Item
-{
-public:
+typedef enum {
+  ITEM_FILE,
+  ITEM_DIRECTORY
+} ItemType;
+
+class Item {
+  private:
+    std::string name;
+    ItemType type;
+  public:
     Item();
+    Item(std::string name, ItemType type);
+    std::string getName();
+    ItemType getType();
+    void setName(std::string name);
+    void setType(ItemType type);
 };
 
 #endif // ITEM_H
