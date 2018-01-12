8d252552fa8623d83ac9eb243855749dc17893da
Marek Felsoci <marek.felsoci@gmail.com>
2018-01-12 23:41:58 +0100

    Implementing constructor

--- filer.cpp
+++ filer.cpp
@@ -1,6 +1,21 @@
 #include "filer.h"
 
-Filer::Filer()
-{
+Filer::Filer() {}
 
+Filer::Filer(QString root) {
+  this->root = root;
+  this->directoriesToProcess = 0;
+  this->filesToProcess = 0;
+
+  QDirIterator i(this->root, QDirIterator::Subdirectories);
+
+  while(i.hasNext()) {
+    QFileInfo current(i.next());
+
+    if(current.isDir()) {
+      this->directoriesToProcess++;
+    } else {
+      this->filesToProcess++;
+    }
+  }
 }
--- filer.h
+++ filer.h
@@ -1,11 +1,21 @@
 #ifndef FILER_H
 #define FILER_H
 
+#include <QString>
+#include <QDebug>
+#include <QDirIterator>
+#include <QFileInfo>
 
 class Filer
 {
+  private:
+    QString root;
+    int directoriesToProcess,
+      filesToProcess,
+      currentlyProcessing;
   public:
     Filer();
+    Filer(QString root);
 };
 
 #endif // FILER_H
