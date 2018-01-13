d9b212fbc23f9fface91ea4c51ddd10e5e80c75c
Marek Felsoci <marek.felsoci@gmail.com>
2018-01-13 01:11:51 +0100

    Simple copy seems to work

--- filer.h
+++ filer.h
@@ -1,21 +1,34 @@
 #ifndef FILER_H
 #define FILER_H
 
+#include <QThread>
 #include <QString>
 #include <QDebug>
+#include <QDir>
+#include <QFile>
 #include <QDirIterator>
 #include <QFileInfo>
 
-class Filer
+class Filer : public QThread
 {
+  Q_OBJECT
   private:
-    QString root;
+    QString root,
+      target,
+      currentlyProcessed;
     int directoriesToProcess,
       filesToProcess,
-      currentlyProcessing;
+      alreadyProcessed;
   public:
-    Filer();
-    Filer(QString root);
+    explicit Filer(QObject *parent = 0);
+    void run();
+    void computeStats();
+    int getDirectoriesToProcess();
+    int getFilesToProcess();
+    void setRoot(QString root);
+    void setTarget(QString target);
+  signals:
+    void currentStatus(int processed, QString current);
 };
 
 #endif // FILER_H
