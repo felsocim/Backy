790ce09f70a903c395acdfc7a9f1271a0e97f1e3
Marek Felsoci <marek.felsoci@gmail.com>
2018-01-20 02:50:48 +0100

    Adapting main for producer and consumer classes debugging purposes

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,39 @@
 #include "interface.h"
+#include "producer.h"
+#include "consumer.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    Interface w;
-    w.show();
+    //QApplication a(argc, argv);
 
-    return a.exec();
+    std::queue<Item> buff;
+    QFileInfo root("/home/marek/Dokumenty/projekty");
+    buff.push(Item(TYPE_DIRECTORY, root.fileName(), root.filePath(), root.lastModified(), root.size()));
+    QMutex lock;
+    QWaitCondition notEmpty, notFull;
+
+
+    Producer prod(&buff, &lock, &notEmpty, &notFull, 5, "eventP.log", "errorP.log");
+
+    prod.setRoot(QString::fromUtf8("/home/marek/Dokumenty/projekty"));
+    prod.analyze();
+
+    if(prod.getDirectoriesCount() + prod.getFilesCount() > 0) {
+      Consumer cons(&buff, &lock, &notEmpty, &notFull, 5, "eventC.log", "errorC.log");
+      cons.setDetectedCount(prod.getDirectoriesCount() + prod.getFilesCount());
+      prod.start();
+      cons.start();
+      prod.wait();
+      cons.wait();
+    }
+
+    //Interface w;
+    //w.show();
+
+    qDebug() << "Ended";
+
+    return 0;
+
+    //return a.exec();
 }
