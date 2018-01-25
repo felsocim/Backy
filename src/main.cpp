d298a4ab4f0cefd338a6492187c17ab6880f5eea
Marek Felsoci <marek.felsoci@gmail.com>
2018-01-25 16:13:09 +0100

    Removing former test code

--- src/main.cpp
+++ src/main.cpp
@@ -5,38 +5,10 @@
 
 int main(int argc, char *argv[])
 {
-    //QApplication a(argc, argv);
+    QApplication a(argc, argv);
 
-    std::queue<Item> buff;
-    QMutex lock;
-    QWaitCondition notEmpty, notFull;
+    Interface w;
+    w.show();
 
-
-    Producer prod(&buff, &lock, &notEmpty, &notFull, 5, "eventP.log", "errorP.log");
-
-    prod.setRoot(QString::fromUtf8("/home/marek/Dokumenty/projekty"));
-    prod.analyze();
-
-    if(prod.getDirectoriesCount() + prod.getFilesCount() > 0) {
-      Consumer cons(&buff, &lock, &notEmpty, &notFull, 5, "eventC.log", "errorC.log");
-      cons.setDetectedCount(prod.getDirectoriesCount() + prod.getFilesCount());
-      cons.setSource("/home/marek/Dokumenty/projekty");
-      cons.setTarget("/home/marek/test");
-      cons.setKeepObsolete(false);
-      cons.setSynchronize(true);
-      cons.setCriterion(CRITERION_MORE_RECENT);
-      prod.start();
-      cons.start();
-      prod.wait();
-      cons.wait();
-    }
-
-    //Interface w;
-    //w.show();
-
-    qDebug() << "Ended";
-
-    return 0;
-
-    //return a.exec();
+    return a.exec();
 }
