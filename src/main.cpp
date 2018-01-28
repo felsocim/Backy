f05f5cbcaf5ead6bdbe1d3500c5a27d759bc3d2e
Marek Felsoci <marek.felsoci@gmail.com>
2018-01-28 01:54:52 +0100

    Setting application properties

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,16 @@
 #include "producer.h"
 #include "consumer.h"
 #include <QApplication>
+#include <QCoreApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QCoreApplication::setApplicationName("Backy");
+    QCoreApplication::setOrganizationName("Marek Felsoci");
+    QCoreApplication::setOrganizationDomain("marekonline.eu");
+    QCoreApplication::setApplicationVersion("1.0");
 
     Interface w;
     w.show();
