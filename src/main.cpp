f5d00adf6234825d742261d718bf75b421a17d5d
Marek Felsoci <marek.felsoci@gmail.com>
2018-02-08 18:06:47 +0100

    Correcting organization name

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     QCoreApplication::setApplicationName("Backy");
-    QCoreApplication::setOrganizationName("Marek Felsoci");
+    QCoreApplication::setOrganizationName("Feldev");
     QCoreApplication::setOrganizationDomain("marekonline.eu");
     QCoreApplication::setApplicationVersion("1.0");
 
