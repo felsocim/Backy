2b1406a03cd4e4610abade603a27d6856aa51df6
Marek Felsoci <marek.felsoci@gmail.com>
2018-06-30 01:17:59 +0200

    Renamed 'isset' function to 'isSet' and implemented 'isSetOrDefault' function

--- docs/src/common.js
+++ docs/src/common.js
@@ -1,3 +1,9 @@
-function isset(variable) {
+function isSet(variable) {
   return variable === null || variable === undefined;
 }
+
+function isSetOrDefault(variable, value) {
+  return isSet(variable) ? value : variable;
+}
+
+export { isSet, isSetOrDefault };
