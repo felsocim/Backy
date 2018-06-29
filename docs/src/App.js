0b225d27223affe914aa56b40787e0b9249866c8
Marek Felsoci <marek.felsoci@gmail.com>
2018-06-30 01:20:19 +0200

    Testing site navigation component

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './Navigation';
 
 class App extends Component {
+  testHandler() {
+    alert("Giducová");
+  }
   render() {
     return (
       <div className="App">
+        <Navigation handler={this.testHandler.bind(this)}/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Vitajte na stránke projektu Backy!</h1>
--- docs/src/Navigation.js
+++ docs/src/Navigation.js
@@ -1,14 +1,35 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import 'semantic-ui-css/semantic.min.css';
+import { Menu } from 'semantic-ui-react';
+import { isSetOrDefault } from './common';
+import { defaultLocale, translate } from "./strings";
 
 class Navigation extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fullFormat: this.props.fullFormat
+      scrolled: isSetOrDefault(this.props.scrolled, false),
+      locale: isSetOrDefault(this.props.locale, defaultLocale)
     };
   }
 
+  render() {
+    return (
+      <Menu stackable>
+        <Menu.Item
+          onClick={this.props.handler}
+        >
+          { translate('NAVIGATION_HOME') }
+        </Menu.Item>
+      </Menu>
+    );
+  }
 }
 
-Navigation.PropTypes
+Navigation.PropTypes = {
+  scrolled: PropTypes.bool,
+  locale: PropTypes.string
+};
+
+export default Navigation;
